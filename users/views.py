269afd6e1392f433a23b9f4ba3a8281807e0b824
joel110399 <peraltajoel88@gmail.com>
2022-05-16 13:20:07 -0400

    Update resume

--- users/views.py
+++ users/views.py
@@ -1,6 +1,6 @@
 # Django
 from users.models import Cliente, Gasto, Categoria, Ingreso
-from django.db.models import Count, Sum
+from django.db.models import Sum
 from django.utils import timezone
 
 
@@ -10,10 +10,9 @@ from rest_framework.response import Response
 
 # others
 import requests
-from datetime import date, datetime, timedelta
+from datetime import date, timedelta
 from datetime import timedelta
 from dateutil.relativedelta import relativedelta
-
 
 
 def registrar_gasto(url, headers, cliente, categoria, monto, mensaje):
@@ -51,9 +50,7 @@ def registrar_ingreso(url, headers, cliente, categoria, monto):
 
     try:
         if isinstance(monto, list):
-            # print(monto)
             monto = monto[0]
-        # print(monto)
         ingreso = Ingreso()
         ingreso.cliente = cliente
         ingreso.categoria = categoria
@@ -80,7 +77,7 @@ def registrar_ingreso(url, headers, cliente, categoria, monto):
     # return Response(status=200)
 
 
-def get_resumen(url, headers, cliente, periodo, tipo):
+def get_resumen(url, headers, cliente, periodo):
     print('resumen')
 
     if periodo == 'diarios':
@@ -96,73 +93,53 @@ def get_resumen(url, headers, cliente, periodo, tipo):
     else:
         periodo = date.today()
 
+    try:
+        result = (Gasto.objects
+                    .filter(cliente=cliente, fecha__range=(periodo, date.today()))
+                    .values('categoria__nombre')
+                    .annotate(monto=Sum('monto'))
+                    .order_by()
+                    )
 
-    if tipo == 'resumen-gastos':
-        try:
-            result = (Gasto.objects
-                .filter(cliente=cliente, fecha__range=(periodo, date.today()))
-                .values('categoria__nombre')
-                .annotate(monto=Sum('monto'))
-                .order_by()
-            )
-        
-            mensaje = []
-            for i in result:
-                # print(f'{i["categoria__nombre"]} - {i["monto"]}')
-                mensaje.append(f'{i["categoria__nombre"]} - {i["monto"]}' + '\n')
+        result_ingreso = (Ingreso.objects
+                    .filter(cliente=cliente, fecha__range=(periodo, date.today()))
+                    .values('categoria__nombre')
+                    .annotate(monto=Sum('monto'))
+                    .order_by()
+                    )
 
-            s=""""""
+        mensaje = []
+        total = 0
+        total_ingreso = 0
 
-            for m in mensaje:
-                s += m         
+        for i in result:
+            mensaje.append(
+                f'       {i["categoria__nombre"]} - {i["monto"]}' + '\n')
+            total = total + i['monto']
 
+        for i in result_ingreso:
+            print(i)
+            total_ingreso = total_ingreso + i['monto']
 
-            payload = {"body": [
-                {
-                    "type": "text",
-                    "text": f'{s}'
-                }
-            ]}
-        except:
-            payload = {"body": [
-                {
-                    "type": "text",
-                    "text": "No pudiomos obtener su resumen."
-                }
-            ]}
-    elif tipo == 'resumen-ingresos':
-        try:
-            result = (Ingreso.objects
-                .filter(cliente=cliente, fecha__range=(periodo, date.today()))
-                .values('categoria__nombre')
-                .annotate(monto=Sum('monto'))
-                .order_by()
-            )
-        
-            mensaje = []
-            for i in result:
-                # print(f'{i["categoria__nombre"]} - {i["monto"]}')
-                mensaje.append(f'{i["categoria__nombre"]} - {i["monto"]}' + '\n')
+        s = """"""
 
-            s=""""""
-
-            for m in mensaje:
-                s += m         
+        for m in mensaje:
+            s += m
 
 
-            payload = {"body": [
-                {
-                    "type": "text",
-                    "text": f'{s}'
-                }
-            ]}
-        except:
-            payload = {"body": [
-                {
-                    "type": "text",
-                    "text": "No pudiomos obtener su resumen."
-                }
-            ]}
+        payload = {"body": [
+            {
+                "type": "text",
+                "text": f'👇 Este es su resumen desde {periodo} hasta {date.today()} 👇 \n\n{s} \n\nEl total de tus gastos es de: {total}\n\nEl total de tus ingresos es de: {total_ingreso}'
+            }
+        ]}
+    except:
+        payload = {"body": [
+            {
+                "type": "text",
+                "text": "No pudiomos obtener su resumen."
+            }
+        ]}
 
     response = requests.request("POST", url, json=payload, headers=headers)
 
@@ -182,8 +159,8 @@ class list_parameters(views.APIView):
         print('   ')
         print('   ')
         print(request.data)
-        telegram_token = '3de05bd610c883cae1c7f27c924071543ce73662f0319b996f9f6991176b9e6a5a649f9962137504b68b0b66f1cfe6434e5151809a314ea1c0651c4fcb4be208'
-        ws_token = 'efaa72594d1aa8a7122be3c49b65c8a7b65f4b943744c79015eb0c64689500b28d4fd15a435a0b5ff7802c1c963abde3983786e9a83a027954d448cfd898729d'
+        # telegram_token = '3de05bd610c883cae1c7f27c924071543ce73662f0319b996f9f6991176b9e6a5a649f9962137504b68b0b66f1cfe6434e5151809a314ea1c0651c4fcb4be208'
+        # ws_token = 'efaa72594d1aa8a7122be3c49b65c8a7b65f4b943744c79015eb0c64689500b28d4fd15a435a0b5ff7802c1c963abde3983786e9a83a027954d448cfd898729d'
         ariel_token = 'f85ef8efec71f0e6c066f9cab1f20a4febad1c9d9f0fb08383b01a8976f7c2585be5edae6186399ec7a7da779074108a456b3038aef4187b0c73c822b1e6c4d6'
 
         headers = {
@@ -191,7 +168,7 @@ class list_parameters(views.APIView):
             "Content-Type": "application/json"
         }
 
-        resumen = True if request.data['queryResult']['action'] == 'resumen-ingresos' or request.data['queryResult']['action'] == 'resumen-gastos' else False
+        resumen = True if request.data['queryResult']['action'] == 'resumen.resumen-custom' else False
 
         if resumen == False:
             category = request.data['queryResult']['parameters']['category']
@@ -218,11 +195,9 @@ class list_parameters(views.APIView):
         else:
             periodo_reporte = request.data['queryResult']['parameters']['periodo-reporte']
 
-
         contact_id = request.data['originalDetectIntentRequest']['payload']['contact']['cId']
         nombre = request.data['originalDetectIntentRequest']['payload']['body']['contacts'][0]['profile']['name']
         tipo = request.data['queryResult']['action']
-
 
         url = f'https://app.respond.io/api/v1/message/sendContent/{contact_id}'
 
@@ -235,13 +210,11 @@ class list_parameters(views.APIView):
             cliente.botcity_id = contact_id
             cliente.save()
 
-
         if resumen:
-            get_resumen(url, headers, cliente, periodo_reporte, tipo)
+            get_resumen(url, headers, cliente, periodo_reporte)
         elif tipo == 'registro-gasto':
             registrar_gasto(url, headers, cliente, categoria, monto, mensaje)
         elif tipo == 'registro-ingresos':
             registrar_ingreso(url, headers, cliente, categoria, monto)
-
 
         return Response(status=200)
